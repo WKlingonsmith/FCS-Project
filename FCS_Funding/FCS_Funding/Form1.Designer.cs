f5f0aa1c3e38d8975b1ef41a257d704c0d369d1e
Spencer <spence521@yahoo.com>
2016-01-20 20:39:05 -0700

    Commit

--- FCS_Funding/FCS_Funding/Form1.Designer.cs
+++ FCS_Funding/FCS_Funding/Form1.Designer.cs
@@ -28,12 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.grantsTab = new System.Windows.Forms.TabControl();
+            this.clientsTab = new System.Windows.Forms.TabPage();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.grantsTab.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grantsTab
+            // 
+            this.grantsTab.Controls.Add(this.clientsTab);
+            this.grantsTab.Controls.Add(this.tabPage1);
+            this.grantsTab.Controls.Add(this.tabPage2);
+            this.grantsTab.Controls.Add(this.tabPage3);
+            this.grantsTab.Controls.Add(this.tabPage4);
+            this.grantsTab.Controls.Add(this.tabPage5);
+            this.grantsTab.Location = new System.Drawing.Point(12, 12);
+            this.grantsTab.Name = "grantsTab";
+            this.grantsTab.SelectedIndex = 0;
+            this.grantsTab.Size = new System.Drawing.Size(916, 388);
+            this.grantsTab.TabIndex = 1;
+            // 
+            // clientsTab
+            // 
+            this.clientsTab.Location = new System.Drawing.Point(4, 25);
+            this.clientsTab.Name = "clientsTab";
+            this.clientsTab.Padding = new System.Windows.Forms.Padding(3);
+            this.clientsTab.Size = new System.Drawing.Size(908, 359);
+            this.clientsTab.TabIndex = 1;
+            this.clientsTab.Text = "Clients";
+            this.clientsTab.UseVisualStyleBackColor = true;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(908, 359);
+            this.tabPage1.TabIndex = 2;
+            this.tabPage1.Text = "Grants";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(908, 359);
+            this.tabPage2.TabIndex = 3;
+            this.tabPage2.Text = "Donors";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(908, 359);
+            this.tabPage3.TabIndex = 4;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 25);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(908, 359);
+            this.tabPage4.TabIndex = 5;
+            this.tabPage4.Text = "tabPage4";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Location = new System.Drawing.Point(4, 25);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage5.Size = new System.Drawing.Size(908, 359);
+            this.tabPage5.TabIndex = 6;
+            this.tabPage5.Text = "tabPage5";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(929, 405);
+            this.Controls.Add(this.grantsTab);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.grantsTab.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl grantsTab;
+        private System.Windows.Forms.TabPage clientsTab;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
+
     }
 }
 
--- FCS_Funding/FCS_Funding/Form1.cs
+++ FCS_Funding/FCS_Funding/Form1.cs
@@ -13,8 +13,10 @@ namespace FCS_Funding
     public partial class Form1 : Form
     {
         public Form1()
-        {
+        {           
             InitializeComponent();
         }
+
+       
     }
 }
