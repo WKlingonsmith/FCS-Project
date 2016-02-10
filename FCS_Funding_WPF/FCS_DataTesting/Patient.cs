9cdca00102b1be9ceebcff637e55ef74cd989951
Spencer <spence521@yahoo.com>
2016-02-10 10:41:58 -0700

    no message

---
+++ FCS_Funding_WPF/FCS_DataTesting/Patient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FCS_DataTesting
+{
+    public class Patient
+    {
+        public string Name { get; private set; }
+        public int PatientOQ { get; private set; }
+        public string Notes { get; private set; }
+
+        public Patient(string n, int p, string note)
+        {
+            Name = n;
+            PatientOQ = p;
+            Notes = note;
+        }
+    }
+}
