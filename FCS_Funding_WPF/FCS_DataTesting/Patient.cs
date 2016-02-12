c73168c04388362b1fe80f36e48a918e427169c4
Spencer <spence521@yahoo.com>
2016-02-11 23:39:59 -0700

    no message

--- FCS_Funding_WPF/FCS_DataTesting/Patient.cs
+++ FCS_Funding_WPF/FCS_DataTesting/Patient.cs
@@ -8,14 +8,29 @@ namespace FCS_DataTesting
 {
     public class Patient
     {
-        public string Name { get; private set; }
-        public int PatientOQ { get; private set; }
-        public string Notes { get; private set; }
+        public int PatientOQ { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public string AgeGroup { get; set; }
+        public string Ethnicity { get; set; }
+        public DateTime Time { get; set; }
+        public Boolean IsHead { get; set; }
+        public string RelationToHead { get;  set; }
+        public string Notes { get; set; }
 
-        public Patient(string n, int p, string note)
+        public Patient(int p, string fn, string ln, string g, string ag, string e, DateTime t, Boolean h, string rth,  string note)
         {
-            Name = n;
             PatientOQ = p;
+            FirstName = fn;
+            LastName = ln;
+            Gender = g;
+            AgeGroup = ag;
+            Ethnicity = e;
+            Time = t;
+            IsHead = h;
+            RelationToHead = rth;
+
             Notes = note;
         }
     }
