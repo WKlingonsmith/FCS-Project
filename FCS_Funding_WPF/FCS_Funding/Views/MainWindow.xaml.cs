8f9b075f4c8ead0178b3a3e8379923da2c8ba9d7
Stephenicus <stephenatkin@mail.weber.edu>
2016-04-16 13:06:06 -0600

    Removed a ton of unused libraries

--- FCS_Funding_WPF/FCS_Funding/Views/MainWindow.xaml.cs
+++ FCS_Funding_WPF/FCS_Funding/Views/MainWindow.xaml.cs
@@ -1,22 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
+//using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//using System.Text;
+//using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+//using System.Windows.Data;
+//using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+//using System.Windows.Media;
+//using System.Windows.Media.Imaging;
+//using System.Windows.Navigation;
+//using System.Windows.Shapes;
 using FCS_Funding.Views;
 using FCS_DataTesting;
 using System.Collections.ObjectModel;
-using FCS_Funding.Models;
-using System.Windows.Automation.Peers;
+//using FCS_Funding.Models;
+//using System.Windows.Automation.Peers;
 
 namespace FCS_Funding
 {
--- FCS_Funding_WPF/FCS_Funding/Views/UpdateIndividualDonor.xaml.cs
+++ FCS_Funding_WPF/FCS_Funding/Views/UpdateIndividualDonor.xaml.cs
@@ -1,17 +1,17 @@
 ﻿
-using System;
-using System.Collections.Generic;
+//using System;
+//using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//using System.Text;
+//using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+//using System.Windows.Data;
+//using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+//using System.Windows.Media;
+//using System.Windows.Media.Imaging;
+//using System.Windows.Shapes;
 using FCS_DataTesting;
 namespace FCS_Funding.Views
 {
