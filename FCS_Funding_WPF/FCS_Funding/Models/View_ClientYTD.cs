3a8763e485a2e7d4c6d041f30dbac14f999bf78b
wklingonsmith <wklingonsmith@mail.weber.edu>
2016-04-20 21:56:57 -0600

    Forgot to add in this file to the project to help with the new view model

---
+++ FCS_Funding_WPF/FCS_Funding/Models/View_ClientYTD.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FCS_Funding.Models
+{
+    public partial class View_ClientYTD
+    {
+        [Key]
+        [Column(Order = 0)]
+        [StringLength(100)]
+        public int PatientName { get; set; }
+
+        [Key]
+        [Column(Order = 1)]
+        [StringLength(25)]
+        public string PatientOQ { get; set; }
+
+        [Key]
+        [Column(Order = 2)]
+        [DataType(DataType.Currency)]
+        public string PatientTotalCopay { get; set; }
+        
+    }
+}
+
+
